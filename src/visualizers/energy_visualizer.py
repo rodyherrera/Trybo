35c4c69e726671f28273db38d3269da476dc214a
rodyherrera <contact@rodyherrera.com>
2025-05-07 00:06:45 -0400

    feat(visualizers/energy): generates a 3D visualization of atom energy

--- src/visualizers/energy_visualizer.py
+++ src/visualizers/energy_visualizer.py
@@ -106,3 +106,53 @@ class EnergyVisualizer:
         
         plt.show()
     
+    def plot_energy_3d(self, timestep_idx=-1, group=None, energy_type='total'):
+        timesteps = self.parser.get_timesteps()
+        
+        if timestep_idx < 0:
+            timestep_idx = len(timesteps) + timestep_idx
+        
+        data = self.parser.get_data()[timestep_idx]
+        current_timestep = timesteps[timestep_idx]
+        
+        if group is not None and group != 'all':
+            group_indices = self.analyzer.get_atom_group_indices()[group]
+            data = data[group_indices]
+        
+        x = data[:, 2]
+        y = data[:, 3]
+        z = data[:, 4]
+        
+        if energy_type == 'kinetic':
+            energy_col = 5
+            title_prefix = 'Kinetic'
+            cmap = self.energy_cmaps['kinetic']
+        elif energy_type == 'potential':
+            energy_col = 6 
+            title_prefix = 'Potential'
+            cmap = self.energy_cmaps['potential']
+        else:
+            energy_col = 7
+            title_prefix = 'Total'
+            cmap = self.energy_cmaps['total']
+        
+        energy_values = data[:, energy_col]
+        
+        fig = plt.figure(figsize=(12, 10))
+        ax = fig.add_subplot(111, projection='3d')
+        
+        scatter = ax.scatter(x, y, z, c=energy_values, cmap=cmap, s=10, alpha=0.7)
+        
+        cbar = plt.colorbar(scatter, ax=ax, label=f'{title_prefix} Energy (eV)')
+        ax.set_xlabel('X (Å)')
+        ax.set_ylabel('Y (Å)')
+        ax.set_zlabel('Z (Å)')
+        
+        title = f'3D {title_prefix} Energy Distribution (Timestep {current_timestep})'
+        if group is not None and group != 'all':
+            title += f' - Group: {group}'
+        ax.set_title(title)
+        
+        plt.tight_layout()
+        
+        plt.savefig(f'{energy_type}_energy_3d_timestep_{current_timestep}.png', dpi=300)
