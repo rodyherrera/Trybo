dffd1c246c8fe9fa6d995361b8b29c1790408872
rodyherrera <contact@rodyherrera.com>
2025-05-07 00:31:58 -0400

    feat(analyzers/centro_symmetric): identifies atom indices for different groups based on their position in the simulation

--- src/analyzers/centro_symmetric_analyzer.py
+++ src/analyzers/centro_symmetric_analyzer.py
@@ -20,3 +20,25 @@ class CentroSymmetricAnalyzer:
             'surface': (5.0, 8.0),
             'defect': (8.0, float('inf'))
         }
+    
+    def get_atom_group_indices(self):
+        if self._atom_groups is not None:
+            return self._atom_groups
+        data = self.parser.get_data()[0]
+        # x = data[:, 2]
+        # y = data[:, 3]
+        z = data[:, 4]
+        z_min = np.min(z)
+        z_max = np.max(z)
+        z_threshold_lower = z_min + 2.5
+        z_threshold_upper = z_max - 2.5
+        lower_plane_mask = z <= z_threshold_lower
+        upper_plane_mask = z >= z_threshold_upper
+        nanoparticle_mask = ~(lower_plane_mask | upper_plane_mask)
+        self._atom_groups = {
+            'lower_plane': np.where(lower_plane_mask)[0],
+            'upper_plane': np.where(upper_plane_mask)[0],
+            'nanoparticle': np.where(nanoparticle_mask)[0],
+            'all': np.arange(len(data))
+        }
+        return self._atom_groups
